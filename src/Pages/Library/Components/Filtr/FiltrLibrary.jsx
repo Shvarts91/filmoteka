7e017f803853776b6fb0d3cb95572b4e19ea9dd3
Artem <shvachev91@gmail.com>
2023-07-21 13:10:28 +0300

    fix bug

--- src/Pages/Library/Components/Filtr/FiltrLibrary.jsx
+++ src/Pages/Library/Components/Filtr/FiltrLibrary.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { useDispatch } from 'react-redux'
 import { useState } from 'react'
-import { useLocalStorage } from '@uidotdev/usehooks'
+import { useLocalStorage } from '../../../../hooks/useLocalStorage'
 import Stack from '@mui/material/Stack'
 import { fetchFilmListById } from '../../../../api/api'
 import { FiltrButton } from './FiltrButton'
